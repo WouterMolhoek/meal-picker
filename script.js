9565c0ee0dd406e1a46207d98addc1c4194b01c8
Wouter Molhoek <woutermolhoek@hotmail.com>
2019-04-09 18:10:57 +0200

    Search optionis functional

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const mealDB = 'https://www.themealdb.com/api/json/v1/1/random.php';
+const mealDB_random = 'https://www.themealdb.com/api/json/v1/1/random.php';
+const mealDB_search = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
 const title = document.getElementById('meal-title');
 const instr = document.getElementById('meal-instr');
@@ -8,12 +9,14 @@ const imgEl = document.getElementById('meal-img');
 const videoEl = document.getElementById('meal-video');
 
 
-const setupUI = () => {
+const setupUI = (api) => {
 
-  getData()
+  getData(api)
    .then(({name,category,instructions,ingredients,img,video}) =>  {
 
      title.innerHTML = name;
+
+     // Set page title to the recipy name
      document.title = 'Meal Picker : ' + name;
      instr.innerHTML = instructions;
      catagory.innerHTML = category;
@@ -34,11 +37,11 @@ const setupUI = () => {
 }
 
 
-async function getData() {
+async function getData(api) {
 
   title.innerHTML = 'loading...';
 
-  const response = await fetch(mealDB);
+  const response = await fetch(api);
   const json = await response.json();
   const data = json.meals[0];
 
@@ -63,11 +66,20 @@ async function getData() {
   }
 }
 
-setupUI();
+setupUI(mealDB_random);
 
 // Buttons
 const refreshBtn = document.getElementById('refresh-btn');
 const searchBtn = document.getElementById('search-btn');
 
 // Refresh the page
-refreshBtn.addEventListener('click', setupUI);
+refreshBtn.addEventListener('click', () => {
+  setupUI(mealDB_random);
+});
+
+// Search for specefic meal
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const input = document.getElementById('meal-search').value;
+  setupUI(mealDB_search + input);
+});
